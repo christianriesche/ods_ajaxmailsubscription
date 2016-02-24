86c3d419f65f47334a732d76e5c6713a570b423d
Robert Heel <Robert.Heel@gmx.de>
2016-02-24 23:54:48 +0100

    * Bugfix: Add flexform data move to new update script.

--- class.ext_update.php
+++ class.ext_update.php
@@ -66,6 +66,7 @@ class ext_update {
 	protected function processUpdates() {
 		$this->addDatabaseField('fe_users', 'tx_odsajaxmailsubscription_rid', "varchar(8) DEFAULT '' NOT NULL");
 		$this->addDatabaseField('tt_address', 'tx_odsajaxmailsubscription_rid', "varchar(8) DEFAULT '' NOT NULL");
+		$this->moveFieldFFtoTable('sDEF','page_records','tt_content','pages');
 	}
 	
 	/**
@@ -99,6 +100,85 @@ class ext_update {
 
 		$this->messageArray[] = array($status, $title, $message);
 		return $status;
+	}
+	
+	protected function moveFieldFFtoTable($FFsheet,$FFfield,$DBtable,$DBfield) {
+		$title = 'Move data from flexform sheet "' . $FFsheet . '", field "' . $FFfield . '" to table "' . $DBtable . '", field "' . $DBfield . '"';
+		$status = t3lib_FlashMessage::OK;
+	
+		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
+			'*',
+			'tt_content',
+			'CType=' . $GLOBALS['TYPO3_DB']->fullQuoteStr('list', 'tt_content') . ' AND list_type=' . $GLOBALS['TYPO3_DB']->fullQuoteStr('ods_ajaxmailsubscription_pi1', 'tt_content') . ' AND deleted=0'
+		);
+
+		if ($res) {
+			$flexObj = t3lib_div::makeInstance('t3lib_flexformtools');
+		
+			while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+				$xmlArr = t3lib_div::xml2array($row['pi_flexform']);
+				$oldVal = $this->pi_getFFvalue($xmlArr, $FFfield, $FFsheet);
+				if ($oldVal) {
+					$message = 'Moved value "' . $oldVal . '" on content id "' . $row['uid'] . '"' . "\n";
+					unset($xmlArr['data'][$FFsheet]['lDEF'][$FFfield]);
+
+					$newff = $flexObj->flexArray2Xml($xmlArr, 1);
+
+					$fields_values = array('pi_flexform' => $newff);
+					if($DBtable=='tt_content') $fields_values[$DBfield]=$oldVal;
+					$UPDATEres = $GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+						'tt_content',
+						'uid=' . $row['uid'],
+						$fields_values
+					);
+					if (!$UPDATEres) {
+						$status = t3lib_FlashMessage::ERROR;
+					}
+					
+				}
+			}
+			if(empty($message)) $message='No data to move.';
+			$this->messageArray[] = array($status, $title, $message);
+		}
+	}
+
+	protected function pi_getFFvalue($T3FlexForm_array, $fieldName, $sheet = 'sDEF', $lang = 'lDEF', $value = 'vDEF') {
+		$sheetArray = is_array($T3FlexForm_array) ? $T3FlexForm_array['data'][$sheet][$lang] : '';
+		if (is_array($sheetArray)) {
+			return $this->pi_getFFvalueFromSheetArray($sheetArray, explode('/', $fieldName), $value);
+		}
+	}
+
+	/**
+	 * Returns part of $sheetArray pointed to by the keys in $fieldNameArray
+	 *
+	 * @param	array		Multidimensiona array, typically FlexForm contents
+	 * @param	array		Array where each value points to a key in the FlexForms content - the input array will have the value returned pointed to by these keys. All integer keys will not take their integer counterparts, but rather traverse the current position in the array an return element number X (whether this is right behavior is not settled yet...)
+	 * @param	string		Value for outermost key, typ. "vDEF" depending on language.
+	 * @return	mixed		The value, typ. string.
+	 * @access private
+	 * @see pi_getFFvalue()
+	 */
+	protected function pi_getFFvalueFromSheetArray($sheetArray, $fieldNameArr, $value) {
+		$tempArr = $sheetArray;
+		foreach ($fieldNameArr as $k => $v) {
+			if (is_int($v)) {
+				if (is_array($tempArr)) {
+					$c = 0;
+					foreach ($tempArr as $values) {
+						if ($c == $v) {
+							#debug($values);
+							$tempArr = $values;
+							break;
+						}
+						$c++;
+					}
+				}
+			} else {
+				$tempArr = $tempArr[$v];
+			}
+		}
+		return $tempArr[$value];
 	}
 }
 ?>
