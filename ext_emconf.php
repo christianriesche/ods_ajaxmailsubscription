cac08d9e58b974cfff6be9700070b0dc5bb400ca
Christian Riesche <christian.riesche@dmk-ebusiness.de>
2017-11-30 13:07:09 +0100

    compatibility to typo3 8.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Ajax mail subscription',
 	'description' => 'Adds a plugin for subscription to direct mail newsletters',
 	'category' => 'plugin',
-	'version' => '1.6.2',
+	'version' => '2.0.0',
 	'state' => 'stable',
 	'uploadfolder' => true,
 	'createDirs' => '',
@@ -22,17 +22,17 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Robert Heel',
 	'author_email' => 'typo3@bobosch.de',
 	'author_company' => 'http://www.1drop.de/',
-	'constraints' => 
+	'constraints' =>
 	array (
-		'depends' => 
+		'depends' =>
 		array (
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-8.79.99',
 			'tt_address' => '3.0.0-',
 		),
-		'conflicts' => 
+		'conflicts' =>
 		array (
 		),
-		'suggests' => 
+		'suggests' =>
 		array (
 			'direct_mail' => '',
 		),
--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,10 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:ods_ajaxmailsubscription/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] ='pi_flexform';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1'] ='pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:'.$_EXTKEY . '/pi1/flexform.xml');
 
 if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_odsajaxmailsubscription_pi1_wizicon"] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY).'pi1/class.tx_odsajaxmailsubscription_pi1_wizicon.php';
